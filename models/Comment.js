7af87158f862bc6c0b93e6bdeec9f610ecdb76a0
Amy Hwang <728amyh@gmail.com>
2021-12-26 14:14:21 -0500

    set the comment model

--- models/Comment.js
+++ models/Comment.js
@@ -3,16 +3,39 @@ const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
-Comment.init(
-  {
-    id: {
-      type: DataTypes.STRING,
-      allowNull: flase
-    }
+Comment.init({
+  id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
   },
-  {
-    sequelize
-  }
-);
+  comment_text: {
+      type: DataTypes.STRING,
+      validate: {
 
+          len: [3]
+      }
+  },
+  user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+          model: 'user',
+          key: 'id'
+      }
+  },
+  post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+          model: 'post',
+          key: 'id'
+      }
+  }
+}, {
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'comment'
+});
 module.exports = Comment;
