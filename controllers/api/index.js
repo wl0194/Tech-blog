08e6ac99980ee29b16f46c00655f2ea303243ea3
Amy Hwang <728amyh@gmail.com>
2022-01-03 22:16:47 -0500

    changed layout

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const userRoutes = require('./api/user-routes');
-const postRoutes = require('./api/post-routes');
-const commentRoutes = require('./api/comment-routes');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
