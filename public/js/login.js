0dffcf40a636a8d969e3c1122ccf9f75ad2729ad
Amy Hwang <728amyh@gmail.com>
2022-01-04 17:25:31 -0500

    added edit/login/logout/signup functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,30 +1,25 @@
-const loginFormHandler = async (event) => {
+async function loginFormHandler(event) {
   event.preventDefault();
 
-  // Collect values from the login form
   const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
-    // Send a POST request to the API endpoint
+  if (username && password) {
     const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
       document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
   }
-};
+}
 
-
-document
-  .querySelector('#login-form')
-  .addEventListener('submit', loginFormHandler);
-
-
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
